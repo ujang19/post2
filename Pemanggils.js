2d72738152c1c10433717dcf3a95166010dc2dfa
ujang19 <34524528+ujang19@users.noreply.github.com>
2017-12-25 14:45:32 +0700

    Update Pemanggils.js

--- Pemanggils.js
+++ Pemanggils.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="/p/the-best-vpn-services-for-2018.html"}
 if(x=="2"){xxx="/p/the-best-vpn-services-for-2018.html"}
 if(x=="3"){xxx="/p/the-best-vpn-services-for-2018.html"}
-if(x=="4"){xxx="/p/the-best-vpn-services-for-2018.html"}a_to_ve[i].href="https://www.idsly.tk"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/p/the-best-vpn-services-for-2018.html"}a_to_ve[i].href="http://www.idsly.tk"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
